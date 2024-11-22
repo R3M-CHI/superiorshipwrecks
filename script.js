a4fbd3f69369a1a3add1ae38c8b38bc01f66fd52
Madison <172339924+akuyenga@users.noreply.github.com>
2024-11-21 21:11:51 -0500

    Auto Scan Update
    This should fix the issue with the popups being cut off. It is in its own branch so that it can be "undone" later. First, the JavaScript has been updated to bind the popups to the layer. Second, the script.js is initalized in the html file.

--- script.js
+++ script.js
@@ -3,3 +3,39 @@ let map = L.map('map').setView([47.7231, 86.9407], 13);
             maxZoom: 1,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
+// Set map bounds to restrict panning
+var bounds = [
+    [46.5, -92.0], // Southwest corner
+    [49.5, -84.0]  // Northeast corner
+];
+map.setMaxBounds(bounds);
+
+// Function to handle each feature
+function onEachFeature(feature, layer) {
+    if (feature.properties) {
+        // Create the popup content
+        var content = `
+            <div>
+                <img style="width: 200px;" src="${feature.properties.image}" alt="Ship Image">
+                <h4>${feature.properties.title}</h4>
+                <p><strong>Year:</strong> ${feature.properties.time}</p>
+                <p>${feature.properties.description}</p>
+            </div>`;
+        
+        // Bind the popup with autoPan enabled
+        layer.bindPopup(content, { 
+            closeButton: true, 
+            autoPan: true, 
+            autoPanPadding: [50, 50] 
+        });
+    }
+}
+
+// Add GeoJSON data to the map
+L.geoJSON(dataset1, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function (feature, latlng) {
+        // Use a custom icon for each feature
+        return L.marker(latlng, { icon: L.icon(feature.properties.icon) });
+    }
+}).addTo(map);
