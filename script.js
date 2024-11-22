ad570575dc8ceb777035626df598cb498fcdac26
Madison <172339924+akuyenga@users.noreply.github.com>
2024-11-21 22:12:43 -0500

    html and js updates

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let map = L.map('map').setView([47.7231, 86.9407], 13);
             maxZoom: 1,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
+        L.control.zoom({
+            position: 'topright' // Adjust position if needed
+        }).addTo(map);
 // Set map bounds to restrict panning
 var bounds = [
     [46.5, -92.0], // Southwest corner
